7feaaef814549c6db42883029ef9f6252d9ef25e
Maxwell Wayne <maxwell.wayne.17@cnu.edu>
2021-11-10 22:05:32 -0500

    Added rotten orange problem, which does a breadth first level  search on a grid

--- src/main/TestDynamic.java
+++ src/main/TestDynamic.java
@@ -1,6 +1,10 @@
 package main;
 
+import java.util.ArrayList;
 import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
 
 public class TestDynamic {
 	
@@ -37,13 +41,7 @@ public class TestDynamic {
     }
 
     public static boolean validPoint(int[][] grid, int i, int j){
-        // Is going down valid
-        // Is going right valid
-        int m = grid.length;
-        int n = grid[0].length;
-        boolean iValid = i >= 0 && i < m;
-        boolean jValid = j >=0 && j < n;
-        return iValid && jValid;
+        return (i >= 0 && i < grid.length) && (j >= 0 && j < grid[i].length);
     }
 	//////////////////////////////////////////////////////////////////////////////////////////////////
     
@@ -66,11 +64,94 @@ public class TestDynamic {
     	return lengths;
     }
     //////////////////////////////////////////////////////////////////////////////////////////////////
-	
+    
+    //////////////////////////////////////////////////////////////////////////////////////////////////
+    // Rotting oranges: 0 = empty, 1 = fresh, 2 = rotten
+    // Every minute, rotten oranges infect adjacent fresh oranges (vertical and horizontal), return -1 if there are oranges that won't rot
+    // Return the minimum number of minutes it takes for all oranges to rot
+    // Use Breadth first level search with queue starting at each first rotten orange and add neighbors to it
+    public static int orangesRotting(int[][] grid) {
+    	
+    	final int EMPTY = 0;
+    	final int FRESH = 1;
+    	final int ROTTEN = 2;
+    	final int IMPOSSIBLE = -1;
+    	
+    	
+    	Queue<Point> q = new LinkedList<>();
+    	
+    	// Iterate through, record number of fresh oranges, push rotten into q
+    	int numFresh = 0;
+    	for (int i = 0; i < grid.length; i++) {
+    		for (int j = 0; j < grid[i].length; j++) {
+    			if(grid[i][j] == FRESH) {
+    				numFresh++;
+    			}
+    			else if(grid[i][j] == ROTTEN) {
+    				q.add(new Point(i,j));
+    			}
+    		}
+    	}
+    	// If no fresh oranges, return 0
+    	if(numFresh == 0) {
+    		return EMPTY;
+    	}
+    	
+    	int minutes = -1;
+    	while(!q.isEmpty()) {
+    		int qSize = q.size();
+    		// Iterate through current level
+    		for(int i = 0; i < qSize; i++) {
+    			Point temp = q.remove();
+    			// Get fresh neighbors
+    			List<Point> neighbors = getNeighbors(grid, temp);
+    			for(Point neighbor : neighbors) {
+    				int x = neighbor.getX();
+    				int y = neighbor.getY();
+    				// Infect fresh neighbors then add them to q
+    				if(grid[x][y] == FRESH) {
+    					grid[x][y] = ROTTEN;
+    					numFresh--;
+    					q.add(neighbor);
+    				}
+    			}
+    		}
+    		// Minute has passed
+    		minutes++;
+    	}
+    	
+    	return (numFresh > 0) ? IMPOSSIBLE : minutes;
+    }
+    
+    public static List<Point> getNeighbors(int[][] grid, Point p){
+    	List<Point> neighbors = new ArrayList<>();
+    	
+    	int i = p.getX();
+    	int j = p.getY();
+    	
+    	// Check left, right, up, and down
+    	if(validPoint(grid, i, j-1)) {
+    		neighbors.add(new Point(i, j-1));
+    	}
+    	if(validPoint(grid, i, j+1)) {
+    		neighbors.add(new Point(i, j+1));
+    	}
+    	if(validPoint(grid, i-1, j)) {
+    		neighbors.add(new Point(i-1, j));
+    	}
+    	if(validPoint(grid, i+1, j)) {
+    		neighbors.add(new Point(i+1, j));
+    	}
+ 
+    	return neighbors;
+    }
+    //////////////////////////////////////////////////////////////////////////////////////////////////
 
 	public static void main(String[] args) {
-		HashSet<Integer> test = allLengths(2, 1, 2);
-		System.out.println(test.size());
+		int[][] grid1 = {{2,1,1},{1,1,0},{0,1,1}};
+		int[][] grid2 = {{2,1,1},{0,1,1},{1,0,1}};
+		int[][] grid3 = {{0,2}};
+		System.out.println(orangesRotting(grid3));
 
 	}
 
